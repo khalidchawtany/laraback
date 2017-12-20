775b4af6d4497c06a82cbbd74b1e82c7cb4e8dac
Kevin <kjdion84@gmail.com>
2017-12-20 18:23:53 -0500

    auth bg

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,13 +1,15 @@
 @extends('laraback::layouts.app')
 
 @section('content')
-    <div class="container h-100">
-        <div class="row h-100 justify-content-center align-items-center">
-            <div class="col-lg-4 col-md-6">
-                <h1 class="display-5 mb-4 text-center">{{ config('app.name') }}</h1>
-                <div class="card">
-                    <div class="card-body">
-                        @yield('form')
+    <div class="bg-light h-100">
+        <div class="container h-100">
+            <div class="row h-100 justify-content-center align-items-center">
+                <div class="col-lg-4 col-md-6">
+                    <h1 class="display-5 mb-4 text-center">{{ config('app.name') }}</h1>
+                    <div class="card">
+                        <div class="card-body">
+                            @yield('form')
+                        </div>
                     </div>
                 </div>
             </div>
