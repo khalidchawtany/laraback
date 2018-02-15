f91f9faef4022da59578ab8981416ff30d3b6fb0
Kevin <kjdion84@gmail.com>
2018-02-15 00:25:28 -0500

    bread model tz

--- resources/bread/stubs/default/model.php
+++ resources/bread/stubs/default/model.php
@@ -3,8 +3,11 @@
 /* bread_model_namespace */
 
 use Illuminate\Database\Eloquent\Model;
+use Kjdion84\Laraback\Traits\Timezone;
 
 class bread_model_class extends Model
 {
+    use Timezone;
+
     protected $fillable = ["/* bread_fillable */"];
 }
