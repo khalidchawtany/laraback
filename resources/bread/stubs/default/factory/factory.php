79479c39723fa15ceda98e51c38dd371b3a62772
khalidchawtany <khalidchawtany@yahoo.com>
2019-05-09 12:17:16 +0300

    use user_id in factory as function

--- resources/bread/stubs/default/factory/factory.php
+++ resources/bread/stubs/default/factory/factory.php
@@ -4,13 +4,15 @@ use Faker\Generator as Faker;
 
 $factory->define(App\bread_model_class::class, function (Faker $faker) {
 
-    $users = App\User::all()->take(5)->pluck('id');
+    /* $users = App\User::all()->take(5)->pluck('id'); */
 
     return [
 
         /* bread_factory */
 
-        'user_id' => $users->random(),
+        'user_id' => function () {
+            return factory(App\User::class)->create()->id;
+        },
 
     ];
 });
