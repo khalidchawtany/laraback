02afd0f38bfb8bff10e3eb7ab7c61cbc8a5d21e3
khalidchawtany <khalidchawtany@yahoo.com>
2019-05-15 15:46:16 +0300

    fix dashboard icon tab name

--- resources/bread/stubs/default/views/components/dashboard.blade.php
+++ resources/bread/stubs/default/views/components/dashboard.blade.php
@@ -1,6 +1,8 @@
-                <li>
-                    <span>bread_model_strings</span>
-                    <a href="#" onclick="switchDashboardMainTab('bread_model_string', '/bread_model_variables')">
-                        <img src="/img/dock/bread_model_variable.svg" alt="bread_model_strings" />
-                    </a>
-                </li>
+                @can('view_bread_model_variable')
+                    <li>
+                        <span>bread_model_strings</span>
+                        <a href="#" onclick="switchDashboardMainTab('bread_model_strings', '/bread_model_variables')">
+                            <img src="/img/dock/bread_model_variable.svg" alt="bread_model_strings" />
+                        </a>
+                    </li>
+                @endcan
