ba8131eea958b8e8d4a392812b3894fbafcc86f2
khalidchawtany <khalidchawtany@yahoo.com>
2019-12-25 11:42:59 +0300

    use the new controller

--- resources/bread/stubs/default/controller/controller.php
+++ resources/bread/stubs/default/controller/controller.php
@@ -18,49 +18,47 @@ class bread_controller_class extends Controller
     public function __construct()
     {
         $this->middleware(['auth', 'user.id']);
-        $this->middleware('permission:view_bread_model_variable', ['only' => ['index', 'list']]);
+        $this->middleware('permission:view_bread_model_variable', ['only' => ['paginate', 'jsonList']]);
         $this->middleware('permission:create_bread_model_variable', ['only' => ['create']]);
         $this->middleware('permission:update_bread_model_variable', ['only' => ['update']]);
         $this->middleware('permission:destroy_bread_model_variable', ['only' => ['destroy']]);
     }
 
-    public function index()
-    {
-        return view('bread_controller_viewbread_model_variables.index');
-    }
-
-    public function list(Request $request)
+    public function paginate(Request $request)
     {
         return QueryBuilder::for(bread_model_class::class)
-        ->allowedFilters("/* bread_fillable */")
-        ->jsonPaginate();
+            ->allowedFilters("/* bread_fillable */")
+            ->jsonPaginate();
+    }
+
+    public function jsonList(Request $request)
+    {
+        $query = bread_model_class::selectRaw('bread_model_variables.name, bread_model_variables.id');
+        if ($request->filled('q')) {
+            $query = $query->where('name', 'like', "{$request->q}%");
+        }
+        return $query->limit(10)->get()->toArray();
     }
 
     protected function create(Storebread_model_class $request)
     {
         $bread_model_variable = bread_model_class::create($request->input());
 
-        return ezReturnSuccessMessage('bread_model_string created successfully!', $bread_model_variable->id);
+        return ezReturnSuccessMessage('bread_model_string created successfully!', $bread_model_variable);
     }
 
-    public function update(Updatebread_model_class $request)
+    public function update(Updatebread_model_class $request, bread_model_class $bread_model_variable)
     {
-    	$bread_model_variable = bread_model_class::findOrFail($request->id);
-
         $bread_model_variable->update($request->input());
 
-        return ezReturnSuccessMessage('bread_model_string updated successfully!');
+        return ezReturnSuccessMessage('bread_model_class updated successfully!', $bread_model_variable);
     }
 
-    public function destroy(Request $request)
+    public function destroy(bread_model_class $bread_model_variable)
     {
+        $bread_model_variable->delete();
 
-    	$bread_model_variable = bread_model_class::findOrFail($request->id);
-
-    	$bread_model_variable->delete();
-
-    	return ezReturnSuccessMessage('bread_model_string removed successfully!');
-
+        return ezReturnSuccessMessage('bread_model_class removed successfully!', $bread_model_variable);
     }
 
 }
