9b3042d447801c802ae6c9990a57f5c9d9b73916
KhalidChawtany <khalidchawtany@yahoo.com>
2023-02-13 19:38:39 +0300

    add logs Activitylog to model

--- resources/bread/stubs/default/model.php
+++ resources/bread/stubs/default/model.php
@@ -3,9 +3,20 @@
 /* bread_model_namespace */
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class bread_model_class extends Model
 {
+    use LogsActivity;
+
+    protected static $logOnlyDirty = true;
+    protected static $submitEmptyLogs = false;
+
     protected $fillable = ["/* bread_fillable */"];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 
 }
