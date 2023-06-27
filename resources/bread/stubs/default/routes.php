a76faa8c4f5275e9e5b5648429366eb37065113e
KhalidChawtany <khalidchawtany@yahoo.com>
2023-06-27 16:21:12 +0300

    update routes stub

--- resources/bread/stubs/default/routes.php
+++ resources/bread/stubs/default/routes.php
@@ -1,7 +1,7 @@
 // bread_model_variable routes
-Route::get('/bread_model_variables', 'bread_controller_routes@index');
-Route::get('/bread_model_variables/list', 'bread_controller_routes@list');
-Route::get('/bread_model_variables/dialog', 'bread_controller_routes@showbread_model_classDialog');
-Route::post('/bread_model_variables/create', 'bread_controller_routes@create');
-Route::post('/bread_model_variables/update', 'bread_controller_routes@update');
-Route::post('/bread_model_variables/destroy', 'bread_controller_routes@destroy');
+Route::get('bread_model_variables', [bread_controller_routes::class, 'index']);
+Route::get('bread_model_variables/list', [bread_controller_routes::class, 'list']);
+Route::get('bread_model_variables/dialog', [bread_controller_routes::class, 'showbread_model_classDialog']);
+Route::post('bread_model_variables/create', [bread_controller_routes::class, 'create']);
+Route::post('bread_model_variables/update', [bread_controller_routes::class, 'update']);
+Route::post('bread_model_variables/destroy', [bread_controller_routes::class, 'destroy']);
