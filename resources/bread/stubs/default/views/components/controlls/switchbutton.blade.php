1496ee6df0d273c701e0af0fc230d60f33b380b5
KhalidChawtany <khalidchawtany@yahoo.com>
2023-06-30 01:04:26 +0300

    add switchbutton control

---
+++ resources/bread/stubs/default/views/components/controlls/switchbutton.blade.php
@@ -0,0 +1,5 @@
+
+                <label for="bread_model_variable_bread_attribute_name">{{ __('bread_attribute_label') }}</label>
+                <input id="bread_model_variable_bread_attribute_name" name="bread_attribute_name" value="<?= $bread_model_variable->bread_attribute_name ?>"
+                    class="easyui-switchbutton" data-options="required: true," checked label="" labelWidth="0" onText="On" offText="Off" >
+
