1be37a456ed93e88231e154a9d274b8c3ab13acb
KhalidChawtany <khalidchawtany@yahoo.com>
2023-09-11 08:18:19 +0300

    fix typo

--- resources/bread/stubs/default/views/components/fields/readonly_foreign_field.blade.php
+++ resources/bread/stubs/default/views/components/fields/readonly_foreign_field.blade.php
@@ -1,7 +1,7 @@
 
 				{
 					field: 'bread_attribute_name',
-                    as: 'bread_attribute_table_name_from_foreign_key.name'
+                    as: 'bread_attribute_table_name_from_foreign_key.name',
                     title: '{{ __('bread_attribute_class_from_foreign_key') }}',
 					width: 50,
 				},
